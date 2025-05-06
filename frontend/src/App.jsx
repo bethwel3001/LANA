1c67a0d8c6ad0a0a0eb2e1a3f1c1b7d6a27a03a7
Bethwel Kiplagat <sjemnyango9@gmail.com>
2025-05-06 21:22:39 +0300

    Added api handling on entry

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,41 @@
+import { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
+import { api } from './utils/api'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
+import ErrorPage from './pages/ErrorPage'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
 export default function App() {
+  // Add global error handler
+  useEffect(() => {
+    const interceptor = api.interceptors.response.use(
+      response => response,
+      error => {
+        if (error.response?.status >= 500) {
+          window.location.href = '/error'
+        }
+        return Promise.reject(error)
+      }
+    )
+
+    return () => {
+      api.interceptors.response.eject(interceptor)
+    }
+  }, [])
+
   return (
     <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </AuthProvider>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/error" element={<ErrorPage />} />
+        </Routes>
+      </AuthProvider>
     </QueryClientProvider>
-
   )
 }
